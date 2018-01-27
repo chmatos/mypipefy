8de701587403742701d5cf31c03b1b6c67d12f80
Christiano Matos <chmatos@chmatos.com>
2018-01-27 18:14:44 -0200

    remove pipes:jsonb from organizations

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -3,6 +3,7 @@
 class Organization < ApplicationRecord
   def self.store(organization: nil)
     return if organization.blank?
+    puts organization['id']
     org = Organization.find_or_initialize_by(id: organization['id'])
     org.update(id: organization['id'], name: organization['name'], created_at: organization['created_at'])
   end
