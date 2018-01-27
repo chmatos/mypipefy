e084239842a962e1f40c178027a8069c25318006
Christiano Matos <chmatos@chmatos.com>
2018-01-27 11:09:20 -0200

    remove pipes:jsonb from organizations

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,7 +6,6 @@ class HomeController < ApplicationController
   def fetch_new_data
     organizations = PipefyService.new.fetch_organizations(ids: [92_858])
     PipefyService.new.store(organizations: organizations)
-    # OrganizationService.new.store(organizations: organizations)
     redirect_to home_index_url
   end
 end
