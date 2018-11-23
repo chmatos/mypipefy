c2a8c7115b9eed88253676f9dfc35d8a7a1d14b4
Luciano Sousa <ls@lucianosousa.net>
2018-11-22 19:55:06 -0500

    Add ranking product endpoint test

--- spec/controllers/api/v1/rankings_controller_spec.rb
+++ spec/controllers/api/v1/rankings_controller_spec.rb
@@ -3,18 +3,34 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::RankingsController, type: :controller do
+  before do
+    request.headers.merge!(authenticated_header)
+  end
+
   describe "GET #show" do
-    let(:product) { create(:produto) }
-
-    before do
-      request.headers.merge!(authenticated_header)
-    end
-
     context "no id passed" do
       it "returns an empty hash" do
         get :show, params: { id: "" }
 
         expect(response.body).to eq("{}")
+      end
+    end
+
+    context "with id passed" do
+      let(:product) { create(:produto) }
+
+      it "returns an empty hash" do
+        get :show, params: { id: product.id }
+
+        expect(response.body).to eq(
+          {
+            product_id: product.id,
+            nome: product.nome,
+            coordenadaX: 0.0,
+            coordenadaY: 0,
+            imagePath: "/uploads/small_logo.png"
+          }.to_json
+        )
       end
     end
   end
